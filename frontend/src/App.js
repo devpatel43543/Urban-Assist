5a1df4584f1a7c9c8753f5c914179884b04b373b
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2025-02-07 17:21:08 -0400

    feat: implement authentication context and protected routes with react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import { AuthProvider } from './context/AuthContext.js';
+import ProtectedRoute from './components/ProtectedRoute.js';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login.js';
+import HomePage from './pages/HomePage.js';
 function App() {
   return (
-    <div>App</div>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/login" element={<ProtectedRoute><Login/></ProtectedRoute>} />
+        </Routes>
+    </AuthProvider>
   )
 }
 
