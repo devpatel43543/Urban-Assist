4d8144821518e78ffda5a52c1ba524533d302815
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2025-02-07 18:25:23 -0400

    feat: add registration page and integrate axios for API calls; configure CORS settings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,12 @@ import ProtectedRoute from './components/ProtectedRoute.js';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login.js';
 import HomePage from './pages/HomePage.js';
+import Register from './pages/Register.js';
 function App() {
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
+        <Route path="/" element={<Register/>} />
         <Route path="/login" element={<ProtectedRoute><Login/></ProtectedRoute>} />
         </Routes>
     </AuthProvider>
