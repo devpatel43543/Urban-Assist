5af68d7e0c8e73907b07459663c55a397f2e2d49
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2025-02-11 14:08:21 -0400

    feat: add Tailwind CSS integration and user profile route; update package dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,14 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login.js';
 import HomePage from './pages/HomePage.js';
 import Register from './pages/Register.js';
-function App() {
+import { UserProfile } from './pages/UserProfile.jsx';
+ function App() {
   return (
     <AuthProvider>
       <Routes>
         <Route path="/" element={<Register/>} />
         <Route path="/login" element={ <Login/>} />
+        <Route path="/user-profile" element={<UserProfile />} />
         <Route path="/homepage" element={<ProtectedRoute><HomePage /> </ProtectedRoute>} />
         </Routes>
     </AuthProvider>
