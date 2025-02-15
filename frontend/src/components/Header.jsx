69b1c3991fef4b5cb43d12c704d389beca9d0f97
devpatel43543 <devpatel43543@gmail.com>
2025-02-15 13:08:58 -0400

    added login button in navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -51,9 +51,28 @@ const Header = () => {
           >
             Contact us
           </Link>
+
+          {/* Login Button */}
+          <Link to={frontendRoutes.LOGIN}>
+            <button className="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
+              {/* Heroicons - Login Solid */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <span>Login</span>
+            </button>
+          </Link>
         </nav>
 
-        {/* Mobile Menu Button */}
         <button
           className="lg:hidden flex flex-col ml-4"
           onClick={() => setIsOpen(!isOpen)}
@@ -64,7 +83,6 @@ const Header = () => {
         </button>
       </div>
 
-      {/* Mobile Navigation */}
       {isOpen && (
         <div className="lg:hidden w-full bg-white shadow-md absolute top-16 left-0 py-4">
           <nav className="flex flex-col items-center space-y-4">
@@ -91,6 +109,25 @@ const Header = () => {
               className={getNavLinkClass(frontendRoutes.HOME)}
             >
               Contact us
+            </Link>
+
+            {/* Mobile Login Button */}
+            <Link to={frontendRoutes.LOGIN}>
+              <button className="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span>Login</span>
+              </button>
             </Link>
           </nav>
         </div>
