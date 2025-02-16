3a89a0cfbdafbdda396cc7ed052533a767b0d436
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2025-02-16 02:49:49 -0400

    Refactor email server configuration and environment variable loading; remove unused code and improve URL handling

--- email/src/index.js
+++ email/src/index.js
@@ -1,9 +1,6 @@
-import dotenv from "dotenv";
+ import "./envConfig.js";
  import { app } from "./app.js";
-import path from "path";
- const nodeEnv = process.env.ENV_MODE || 'development';
- const envFile = path.resolve(process.cwd(), `.env.${nodeEnv}`);
- dotenv.config({ path: envFile });
+ 
  
    
   app.listen(process.env.PORT || 8001, () => {
