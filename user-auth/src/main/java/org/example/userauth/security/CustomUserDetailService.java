ad24e36b7e4e7bb1f60b08062a8aec80e9b16f13
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2025-02-28 03:41:12 -0400

    Add CustomUserDTO for enhanced user details and update authentication logic to include userId in JWT claims

--- user-auth/src/main/java/org/example/userauth/security/CustomUserDetailService.java
+++ user-auth/src/main/java/org/example/userauth/security/CustomUserDetailService.java
@@ -24,10 +24,10 @@ public class CustomUserDetailService implements UserDetailsService {
         if (user == null) {
             throw new UsernameNotFoundException("User not found");
         }      
-        return new org.springframework.security.core.userdetails.User(
+        return new CustomUserDTO(
             user.getEmail(),
             user.getPassword(),
-             
+            user.getId(), // Include userId
             Collections.singletonList(new SimpleGrantedAuthority(user.getRole()))
     );
     }
--- user-auth/src/main/java/org/example/userauth/security/JwtUtil.java
+++ user-auth/src/main/java/org/example/userauth/security/JwtUtil.java
@@ -100,7 +100,10 @@ public class JwtUtil {
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.toList()));
         
-         
+        // Add userId to claims (if using CustomUserDetails)
+        if (userDetails instanceof CustomUserDTO) {
+            claims.put("id", ((CustomUserDTO) userDetails).getUserId());
+        }
 
         return createToken(claims, userDetails.getUsername());
     }
